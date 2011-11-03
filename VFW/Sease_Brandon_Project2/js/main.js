011e8f1923c641f9a162d23881813daa83649ba3
Brandon Sease <brandonfyia@gmail.com>
2011-11-03 00:40:01 -0400

    taking a break. left off on video 5.

--- VFW/Sease_Brandon_Project2/js/main.js
+++ VFW/Sease_Brandon_Project2/js/main.js
@@ -2,57 +2,100 @@
 //VFW 11/11
 //Project 2
 
-// Variables
-
 
 //Functions
 
-function storeItems(id) {
-    var date         = document.getElementById("date").value;
-    var piercingType = document.getElementById("piercingtype").value;
-    var pName        = document.getElementById("pName").value;
-    var cash         = document.getElementById("cash").value;
-    var credit       = document.getElementById("credit").value;
-    var cTip         = document.getElementById("cTip").value;
-    var percent      = document.getElementById("percent").value;
-    var notes        = document.getElementById("notes").value;
-    localStorage.setItem("appDate", date);
-    localStorage.setItem("appPiercingType", itemQty);
-    localStorage.setItem("appPName", pName);
-    localStorage.setItem("appCash", cash);
-    localStorage.setItem("appCredit", credit);
-    localStorage.setItem("appCTip", cTip);
-    localStorage.setItem("appPercent", percent);
-    localStorage.setItem("appNotes", notes);   
-};
+window.addEventListener("DOMContentLoaded", function() {
 
-function getItems() {
-    if (localStorage.getItem("appPName")) {
-        var date         = localStorage.getItem("appDate");
-        var piercingType = localStorage.getItem("appPiercingType");
-        var pName        = localStorage.getItem("appPName");
-        var cash         = localStorage.getItem("appCash");
-        var credit       = localStorage.getItem("appCredit");
-        var cTip         = localStorage.getItem("appCTip");
-        var percent      = localStorage.getItem("appPercent");
-        var notes        = localStorage.getItem("appNotes");
-            var piercings = document.getElementById("piercings");
-            while (piercings.firstChild)
-                piercings.removeChild(piercings.firstChild);
-            var newUL  = document.createElement("ul");
-            var newLis = document.createElement("li");
-            newUL.appendChild(newLis);
-            var liTxt  = document.createTextNode(pName + "was added to local storage.")
-            newLis.appendChild(liTxt);
-            piercings.appendChild(newUL);
-    } else {
-        var pname = "Enter Piercings Name";  
+    //Get element by ID function
+    
+    function $(x) {
+        var element = document.getElementById(x);
+        return element;
+        };
+    
+
+    //Create Select field Element
+    
+    function makeCats() {
+        var formTag = document.getElementsByTagName("form"),
+            selectLi = $("select"),
+            makeSelect = document.createElement("select");
+        makeSelect.setAttribute("id", "pCat");
+        for(var i=0, j=pCats.length; i<j; i++) {
+          var makeOption = document.createElement("option");
+          var optText = pCats[i];
+          makeOption.setAttribute("value", optText);
+          makeOption.innerHTML = optText;
+          makeSelect.appendChild(makeOption);
+        };
+        selectLi.appendChild(makeSelect);
     };
-    document.getElementById("pName").value = pName;
-    document.getElementById()
-};
+    
+    //Store Items    
+    
+    function storeData() {
+        var id           = math.floor(Math.random()*1000000001);
+        var piercing     = {};
+            item.date    = ["Date", $("date").value];
+            item.pCat    = ["Piercing Category", $("pCat").value];
+            item.pName   = ["Piercing Name", $("pName").value];
+            item.cash    = ["Cash", $("cash").value];
+            item.credit  = ["Credit", $("credit").value];
+            item.cTip    = ["Credit Tip", $("Ctip").value];
+            item.percent = ["Percent", $("percent").value];
+            item.notes   = ["Notes", $("notes").value];
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("entry saved")
+    };
+    
+    //Get Items
+    
+    function getItems() {
+        if (localStorage.getItem("appPName")) {
+            var date         = localStorage.getItem("appDate");
+            var piercingType = localStorage.getItem("appPCat");
+            var pName        = localStorage.getItem("appPName");
+            var cash         = localStorage.getItem("appCash");
+            var credit       = localStorage.getItem("appCredit");
+            var cTip         = localStorage.getItem("appCTip");
+            var percent      = localStorage.getItem("appPercent");
+            var notes        = localStorage.getItem("appNotes");
+                var piercings = document.getElementById("piercings");
+                while (piercings.firstChild)
+                    piercings.removeChild(piercings.firstChild);
+                var newUL  = document.createElement("ul");
+                var newLis = document.createElement("li");
+                newUL.appendChild(newLis);
+                var liTxt  = document.createTextNode(pName + "was added to local storage.")
+                newLis.appendChild(liTxt);
+                piercings.appendChild(newUL);
+        } else {
+            var pname = "Enter Piercings Name";  
+        };
+        document.getElementById("pName").value = pName;
+        document.getElementById()
+    };
+    
+    //Clear Items
+    
+    function clearLocal() {
+      clear: localStorage.clear();
+      return false;
+    };
+    
+        //Set Link & Submit Click Events
+    
+    var displayLink = $("displayLink");
+    displayLink.addEventListener("click", getData);
+    var clearLink = $("clear");
+    clearLink.addEventListener("click", clearLocal);
+    var save = $("submit");
+    save.addEventListener("click", storeData);
+    
+    // Variable Defaults
+    
+    var pCats = ["--Select Piercing Category--", "Ear", "Face", "Body", "Genital"];
+    makeCats();
 
-function clearLocal() {
-  clear: localStorage.clear();
-  return false;
-};
+});
