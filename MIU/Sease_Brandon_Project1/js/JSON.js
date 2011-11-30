82e4af5075fd23e487605d654602f9803816c108
Brandon Sease <brandonfyia@gmail.com>
2011-11-30 00:46:05 -0500

    JSON DONE

--- MIU/Sease_Brandon_Project1/js/JSON.js
+++ MIU/Sease_Brandon_Project1/js/JSON.js
@@ -3,17 +3,17 @@
     MIU 11/12
     Project 1&2
 */
-
+    var gCats = ["--Select Firearm Category--", "Revolver Pistol", "Semi-Auto Pistol", "Bolt Rifle", "Semi-Auto Rifle", "Pump Shotgun", "Auto-Loader Shotgun"];
 var json = {
 			"gun1": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Pistol"],
+				"gCat"    : ["Gun Type: ", "Semi-Auto Pistol"],
 				"gMake"   : ["Gun Make: ","SIG Sauer"],
 				"gModel"  : ["Gun Model: ", "P220"],
 				"gCal"    : ["Caliber: ", ".45 ACP"],
 				"notes"   : ["Notes: ", "Elite"]
 			},
 			"gun2": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
+				"gCat"    : ["Gun Type: ", "Semi-Auto Rifle"],
 				"gMake"   : ["Gun Make: ","Daniel Defence"],
 				"gModel"  : ["Gun Model: ", "EZ Car AR 15"],
 				"gCal"    : ["Caliber: ", "5.56"],
@@ -23,127 +23,127 @@ var json = {
 				"gCat"    : ["Gun Type: ", "Auto-Loader Shotgun"],
 				"gMake"   : ["Gun Make: ","Benneli"],
 				"gModel"  : ["Gun Model: ", "M4"],
-				"gCal"    : ["Caliber: ", "12g"],
+				"gCal"    : ["Caliber: ", "12 guage"],
 				"notes"   : ["Notes: ", "Added Rail"]
 			},
 			"gun4": {
-				"gCat"    : ["Gun Type: ", ""],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Revolver Pistol"],
+				"gMake"   : ["Gun Make: ","Smith and Wesson"],
+				"gModel"  : ["Gun Model: ", "Governor"],
+				"gCal"    : ["Caliber: ", ".410 guage"],
+				"notes"   : ["Notes: ", "	"]
 			},
 			"gun5": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Bolt Rifle"],
+				"gMake"   : ["Gun Make: ","Savage"],
+				"gModel"  : ["Gun Model: ", "110 BA"],
+				"gCal"    : ["Caliber: ", ".338 Lapua"],
+				"notes"   : ["Notes: ", "Awesome"]
 			},
 			"gun6": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Pump Shotgun"],
+				"gMake"   : ["Gun Make: ","Mossburg"],
+				"gModel"  : ["Gun Model: ", "500"],
+				"gCal"    : ["Caliber: ", "12 guage"],
+				"notes"   : ["Notes: ", "Chainsaw"]
 			},
 			"gun7": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
+				"gCat"    : ["Gun Type: ", "Semi-Auto Pistol"],
 				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gModel"  : ["Gun Model: ", "XD"],
+				"gCal"    : ["Caliber: ", ".40 S&amp;W"],
+				"notes"   : ["Notes: ", "Tactical"]
 			},
 			"gun8": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Auto-Loader Shotgun"],
+				"gMake"   : ["Gun Make: ","Remington"],
+				"gModel"  : ["Gun Model: ", "1187"],
+				"gCal"    : ["Caliber: ", "12 guage"],
+				"notes"   : ["Notes: ", "Cammo"]
 			},
 			"gun9": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Semi-Auto Pistol"],
+				"gMake"   : ["Gun Make: ","Glock"],
+				"gModel"  : ["Gun Model: ", "19"],
+				"gCal"    : ["Caliber: ", "9mm"],
+				"notes"   : ["Notes: ", "Fourth Gen."]
 			},
 			"gun10": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Semi-Auto Rifle"],
+				"gMake"   : ["Gun Make: ","Remington"],
+				"gModel"  : ["Gun Model: ", "ACR"],
+				"gCal"    : ["Caliber: ", "5.56"],
+				"notes"   : ["Notes: ", ""]
 			},
 			"gun11": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
+				"gCat"    : ["Gun Type: ", "Bolt Rifle"],
+				"gMake"   : ["Gun Make: ","Remington"],
+				"gModel"  : ["Gun Model: ", "700 TP"],
 				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"notes"   : ["Notes: ", "10x Optic"]
 			},
 			"gun12": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Semi-Auto Pistol"],
+				"gMake"   : ["Gun Make: ","SIG Sauer"],
+				"gModel"  : ["Gun Model: ", "P226"],
+				"gCal"    : ["Caliber: ", "9mm"],
+				"notes"   : ["Notes: ", "Tactical Opperations"]
 			},
 			"gun13": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Semi-Auto Pistol"],
+				"gMake"   : ["Gun Make: ","SIG Sauer"],
+				"gModel"  : ["Gun Model: ", "P238"],
+				"gCal"    : ["Caliber: ", ".380"],
+				"notes"   : ["Notes: ", "Equinox"]
 			},
 			"gun14": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Revolver Pistol"],
+				"gMake"   : ["Gun Make: ","Taurus"],
+				"gModel"  : ["Gun Model: ", "Judge"],
+				"gCal"    : ["Caliber: ", ".410 guage"],
+				"notes"   : ["Notes: ", ""]
 			},
 			"gun15": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Pump Shotgun"],
+				"gMake"   : ["Gun Make: ","Remington"],
+				"gModel"  : ["Gun Model: ", "870"],
+				"gCal"    : ["Caliber: ", "12 guage"],
+				"notes"   : ["Notes: ", "Rails"]
 			},
 			"gun16": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Semi-Auto Pistol"],
+				"gMake"   : ["Gun Make: ","FNP"],
+				"gModel"  : ["Gun Model: ", "45 Tactical"],
+				"gCal"    : ["Caliber: ", ".45 ACP"],
+				"notes"   : ["Notes: ", "Red Dot Sight"]
 			},
 			"gun17": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Revolver Pistol"],
+				"gMake"   : ["Gun Make: ","Colt"],
+				"gModel"  : ["Gun Model: ", "Sherrif/'s 45"],
+				"gCal"    : ["Caliber: ", ".45 Colt"],
+				"notes"   : ["Notes: ", "2 inch"]
 			},
 			"gun18": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
+				"gCat"    : ["Gun Type: ", "Bolt Rifle"],
+				"gMake"   : ["Gun Make: ","Blaser"],
+				"gModel"  : ["Gun Model: ", "R-93"],
+				"gCal"    : ["Caliber: ", ".300 Win Mag"],
 				"notes"   : ["Notes: ", "First Gen."]
 			},
 			"gun19": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Semi-Auto Rifle"],
+				"gMake"   : ["Gun Make: ","Noveski"],
+				"gModel"  : ["Gun Model: ", "AR 15"],
+				"gCal"    : ["Caliber: ", ".300 BLK"],
+				"notes"   : ["Notes: ", "SBR"]
 			},
 			"gun20": {
-				"gCat"    : ["Gun Type: ", "Simi-Auto Rifle"],
-				"gMake"   : ["Gun Make: ","Springfield"],
-				"gModel"  : ["Gun Model: ", "M1A Socom 16"],
-				"gCal"    : ["Caliber: ", ".308"],
-				"notes"   : ["Notes: ", "First Gen."]
+				"gCat"    : ["Gun Type: ", "Semi-Auto Pistol"],
+				"gMake"   : ["Gun Make: ","Kimber"],
+				"gModel"  : ["Gun Model: ", "1911"],
+				"gCal"    : ["Caliber: ", ".45 ACP"],
+				"notes"   : ["Notes: ", "Eclipse II"]
 			}
 	
 		};
