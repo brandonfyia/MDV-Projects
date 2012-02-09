0254b57ce0f36cefa68363f79122d6fc7471a6a8
Brandon Sease <brandonfyia@gmail.com>
2012-02-08 22:35:30 -0500

    update

--- ASD/Week 2/js/main.js
+++ ASD/Week 2/js/main.js
@@ -6,37 +6,43 @@
 
 //Functions
 
-$(document).ready(function(){
-
-	var mForm = $("#mainForm");
-	mForm.validate({
-		invalidHandler: function(form, validator){},
-		submitHandler : function(){
-			var data = mForm.serializeArray();
-			console.log(data);
-			storeData(data);
-			}
-		});
+$("#home").live("pageinit", function(){
 
 
+});
+
+$("#addItem").live("pageinit", function(){
+
+    var mForm = $("#mainForm");
+    mForm.validate({
+        invalidHandler: function(form, validator){},
+        submitHandler : function(){
+            var data = mForm.serializeArray();
+            console.log(data);
+            storeData(data);
+        }
+    });
 
     var storeData = function storeData(data) {
-		if (!data.key) {
-	        var id       = Math.floor(Math.random()*1000000001);
-		} else {
-			var id       = data.key;
-		};
-		console.log(item);
+        if (!data.key) {
+            var id       = Math.floor(Math.random()*1000000001);
+        } else {
+            var id       = data.key;
+        };
+        console.log(item);
         var item         = {};
-            item.gCat    = ["Gun Type:", data[0].value];
-            item.gMake   = ["Gun Make:", data[1].value];
-            item.gModel  = ["Gun Model:", data[2].value];
-            item.gCal    = ["Caliber:", data[3].value];
-            item.notes   = ["Notes:", data[4].value];
-			console.log(item);
+        item.gCat    = ["Gun Type:", data[0].value];
+        item.gMake   = ["Gun Make:", data[1].value];
+        item.gModel  = ["Gun Model:", data[2].value];
+        item.gCal    = ["Caliber:", data[3].value];
+        item.notes   = ["Notes:", data[4].value];
+        console.log(item);
         localStorage.setItem(id, JSON.stringify(item));
         alert(item.gMake[1]+" "+item.gModel[1]+" saved.");
     };
+});
+
+$("#display").live("pageinit", function(){
 
     //Get Items
 
@@ -47,11 +53,6 @@ $(document).ready(function(){
             type: "GET",
             dataType: "json",
             success: function(data){
-                for (var n in data) {
-                    var id = Math.floor(Math.random()*100000000000000);
-                    localStorage.setItem(id, JSON.stringify(data[n]));
-                    console.log(data[n]);
-                };
                 getData(data);
             },
             error:function(data){
@@ -71,110 +72,95 @@ $(document).ready(function(){
             var makeLi = $('<li></li>').appendTo("#theList");
             var key = localStorage.key(i);
             var value = localStorage.getItem(key);
-            var obj = JSON.parse(value);
+            var obj = data[0];
             makeLi;
-			getImg(obj.gCat[1], makeLi);
-			$("<h3>"+ obj.gModel[1] +"</h3>").appendTo("#theList li:last");
+            getImg(obj.gCat[1], makeLi);
+            $("<h3>"+ obj.gModel[1] +"</h3>").appendTo("#theList li:last");
             for (var n in obj) {
                 var optSubText = obj[n][0]+" "+obj[n][1];
                 $("#theList li:last").append("<p>"+ optSubText +"</p>");
             };
         };
         $('.itemList').listview('refresh');
-	};
+    };
 
-	//Get Image for correct cat.  !!! Using CSS Sprites !!!
+    //Get Image for correct cat.  !!! Using CSS Sprites !!!
 
-	var getImg = function getImg(catName, makeSubList) {
-		var pixels = 0;
-		if (catName === "Revolver Pistol") {
-			var pixels = 0;
-		} else if (catName === "Semi-Auto Pistol") {
-			var pixels = 80;
-		} else if (catName === "Bolt Rifle") {
-			var pixels = 160;
-		} else if (catName === "Semi-Auto Rifle") {
-			var pixels = 240
-		} else if (catName === "Pump Shotgun") {
-			var pixels = 320
-		} else if (catName === "Auto-Loader Shotgun") {
-			var pixels = 400
-		};
-		$("<img src='img/clear.gif'>").appendTo("#theList li:last").css("background", "url(img/master.gif) -"+pixels+"px 0px");
-	};
+    var getImg = function getImg(catName, makeSubList) {
+        var pixels = 0;
+        if (catName === "Revolver Pistol") {
+            var pixels = 0;
+        } else if (catName === "Semi-Auto Pistol") {
+            var pixels = 80;
+        } else if (catName === "Bolt Rifle") {
+            var pixels = 160;
+        } else if (catName === "Semi-Auto Rifle") {
+            var pixels = 240
+        } else if (catName === "Pump Shotgun") {
+            var pixels = 320
+        } else if (catName === "Auto-Loader Shotgun") {
+            var pixels = 400
+        };
+        $("<img src='img/clear.gif'>").appendTo("#theList li:last").css("background", "url(img/master.gif) -"+pixels+"px 0px");
+    };
 
     //Edit and Delete Functions
 
-	var makeItemLinks = function makeItemLinks(key, makeLi) {
-		var editLink = $("<a href='#addItem'>Edit Firearm</a>").appendTo("#theList li:last").css("display", "block");
+    var makeItemLinks = function makeItemLinks(key, makeLi) {
+        var editLink = $("<a href='#addItem'>Edit Firearm</a>").appendTo("#theList li:last").css("display", "block");
         editLink;
-		editLink.click(editItem());
+        editLink.click(editItem());
         editLink.key = key;
         var deleteLink = $("<a href='#'>Delete Firearm</a>").appendTo("#theList li:last").css("display", "block");
         deleteLink;
         deleteLink.key = key;
         deleteLink.click(deleteItem());
-	};
+    };
 
-	var editItem = function editItem() {
-		var value = localStorage.getItem(this.key);
-		var item = JSON.parse(value);
-		$("#gCat").value = item.gCat[1];
-		$("#gMake").value = item.gMake[1];
-		$("#gModel").value = item.gModel[1];
-		$("#gCal").value = item.gCal[1];
-		$("#notes").value = item.notes[1];
-		save.unbind("click", storeData());
-		$("#submit").value = "Edit Firearm";
-		var editSubmit = $("#submit").click(validate);
-		editSubmit;
-		editSubmit.key = this.key;
-	};
+    var editItem = function editItem() {
+        var value = localStorage.getItem(this.key);
+        var item = JSON.parse(value);
+        $("#gCat").value = item.gCat[1];
+        $("#gMake").value = item.gMake[1];
+        $("#gModel").value = item.gModel[1];
+        $("#gCal").value = item.gCal[1];
+        $("#notes").value = item.notes[1];
+        save.unbind("click", storeData());
+        $("#submit").value = "Edit Firearm";
+        var editSubmit = $("#submit").click(validate);
+        editSubmit;
+        editSubmit.key = this.key;
+    };
 
     //Clear Items
 
     var clearLocal = function clearLocal() {
-		if(localStorage.length === 0) {
+        if(localStorage.length === 0) {
             alert("No Firearms Saved.");
         } else {
-           var ask = confirm("Are you sure you want to remove ALL firearms?");
-			if(ask) {
-				localStorage.clear();
-				$.mobile.changePage($("#home"), { transition: "pop"});
-				alert("All Firearms Cleared");
-				return false;
-			}else {
-				alert("Firearms Still Saved.");
+            var ask = confirm("Are you sure you want to remove ALL firearms?");
+            if(ask) {
+                localStorage.clear();
+                $.mobile.changePage($("#home"), { transition: "pop"});
+                alert("All Firearms Cleared");
+                return false;
+            }else {
+                alert("Firearms Still Saved.");
             }
         }
     };
 
-	var deleteItem = function deleteItem () {
-		var ask = confirm("Are you sure you want to remove this Firearm?");
-		if(ask) {
-			localStorage.removeItem(this.key);
-			window.location.reload();
-		}else {
-			alert("Firearm Saved.");
+    var deleteItem = function deleteItem () {
+        var ask = confirm("Are you sure you want to remove this Firearm?");
+        if(ask) {
+            localStorage.removeItem(this.key);
+            window.location.reload();
+        }else {
+            alert("Firearm Saved.");
         }
     };
-
-    //Set Link & Submit Click Events
-
     $("#json").click(getJSON);
     $(".clear").click(clearLocal);
-
-
-    // Variable Defaults
-
-    var items = [];
-
 });
 
-//$.ajax({
-//    url:"xhr/data.json",
-//    type:"GET",
-//    dataType:"json",
-//    success:function(result){console.log(result);},
-//    error:function(result){console.log(result);}
-//});
+//Set Link & Submit Click Events
