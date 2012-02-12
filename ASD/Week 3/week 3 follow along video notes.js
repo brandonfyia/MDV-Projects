51c7e51584d3b9df9b67177602df5be20b862142
Brandon Sease <brandonfyia@gmail.com>
2012-02-12 17:36:07 -0500

    added couch

---
+++ ASD/Week 3/week 3 follow along video notes.js
@@ -0,0 +1,15 @@
+//Brandon Sease
+//ASD 2/12
+//Week 3 follow along
+
+//started video 1.
+//finished video 1.
+//started video 2.
+//installed Couch DB.
+//installed eclipse.
+//installed Couch DB plugin.
+//Edited default couchDB settings per video.
+//finished video 2.
+//started video 3.
+
+
