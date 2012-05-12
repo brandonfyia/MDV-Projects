e39436699b23bdb811e5d70b2700f9baebc06d93
Brandon Sease <brandonfyia@gmail.com>
2012-05-12 19:35:18 -0400

    getting started for the day.

--- AVF/iOS/theguncase/www/js/main.js
+++ AVF/iOS/theguncase/www/js/main.js
@@ -7,12 +7,20 @@
 //Geo Function
 
 $("#geo").live("pageshow", function(){
+
     var didGood = function(position){
-        alert("You are at:"+position)
-    };
+        alert("it works!");
+        var myOptions = {
+            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude ),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+        var map = new google.maps.Map($("#map_canvas"),
+            myOptions);
+    }
     var didBad = function(error){
-        alert("error code: "+ error.code + '/n'+
-              "error message: " + error.message + "/n")
+        alert("error code: "+ error.code + "<br />" +
+              "error message: " + error.message)
     };
     var getGeo = function (){
         navigator.geolocation.getCurrentPosition(didGood, didBad);
